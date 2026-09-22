7556bfd4ceb75ca577436e686c897efa91ff3e6d
m-ragab-elsheikh <mahmoud.ragab.elsheikh@gmail.com>
2026-09-22 13:44:22 +0300

    Fix Vercel base path

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/Merge-Rock-AI" : "",
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
 };
 
